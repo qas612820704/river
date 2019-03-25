db5053d7f8c99f10d441f5607bdeb24a12507c2d
Lego <qas612820704@gmail.com>
2019-03-25 10:32:34 +0800

    change dictionary def card shorter

--- src/pages/dictionary/index.js
+++ src/pages/dictionary/index.js
@@ -1,6 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useMappedState } from 'redux-react-hook';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Button, Badge } from 'antd';
 import { Defination } from '../words/word';
 
 export default function Dictionary() {
@@ -13,17 +13,32 @@ export default function Dictionary() {
       },
       [],
     )
-  )
+  );
 
+  const [ isExpanded, enableExpanded ] = useState(false);
+  const handleExpandedClick = useCallback(
+    () => enableExpanded(!isExpanded),
+    [isExpanded],
+  );
 
   return (
     <Row gutter={16}>
-    { definations.map(def => (
-      <Col key={def.id} span={6}>
-        <Card
-          title={def.word}
-        >
-          <Defination definationId={def.id} />
+    { definations.map(defination => (
+      <Col key={defination.id} span={6}>
+        <Card>
+          <Card.Meta
+            title={
+              <span>
+                <Badge
+                  style={{ marginRight: 8, background: '#52c41a' }}
+                  count={defination.level}
+                />
+                {defination.word}
+              </span>
+            }
+            description={defination.translate}
+          />
+
         </Card>
       </Col>
     ))}
